d18a019de2cd61c9225017d71190e37806e6aa6d
gusmartins499 <gustavosm994@gmail.com>
2021-11-11 17:20:44 +0000

    done todos

--- src/index.js
+++ src/index.js
@@ -72,7 +72,12 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const todo = user.todos.find((todo) => todo.id === id);
+  todo.done = true;
+
+  return response.send();
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
